939f9aca815b0cbc383baacf7c3d798627336372
ttt104524 <67825704+ttt104524@users.noreply.github.com>
2021-09-02 16:32:40 +0800

    Update test.java

--- src/main/java/com/tfy/git/test.java
+++ src/main/java/com/tfy/git/test.java
@@ -9,5 +9,6 @@ public class test {
         System.out.println("修改！！！");
         System.out.println("qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq");
         System.out.println("电脑修改！！！");
+        System.out.println("github更新！！！");
     }
 }
