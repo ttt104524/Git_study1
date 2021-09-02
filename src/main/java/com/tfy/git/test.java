09eb7425908562f215cb96a00f5b1942ed3b6e14
Tangfengyu <1045240209@qq.com>
2021-09-02 15:52:09 +0800

    3333

--- src/main/java/com/tfy/git/test.java
+++ src/main/java/com/tfy/git/test.java
@@ -2,6 +2,6 @@ package com.tfy.git;
 
 public class test {
     public static void main(String[] args) {
-        System.out.println("厉害2222...");
+        System.out.println("3333333333...");
     }
 }
